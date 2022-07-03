5d9252b7368bcba59327087b2e8b989aff15bd71
Pranjal Bansal <78337467+Pranjal-code18@users.noreply.github.com>
2022-07-03 16:55:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import ToDoLists from './ToDoLists';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ToDoLists />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
